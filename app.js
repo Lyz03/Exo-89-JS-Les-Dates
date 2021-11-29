b8785ba63c759ca46f6ff505a61381f7b0a9dd10
Lyz03 <lizoe@netcourrier.com>
2021-11-29 18:16:06 +0100

    exo 89

--- app.js
+++ app.js
@@ -8,6 +8,19 @@ localeTime.innerText = new Date().toLocaleTimeString();
 
 let today = new Date();
 let todayPlus2 = new Date();
+todayPlus2.setDate(today.getDate() + 2)
+todayPlus2.setHours(today.getHours() + 6);
 
-todayPlus2 = todayPlus2.setDate(today.getDate() + 2)
-todayPlus2 = todayPlus2.setDate(today.getDate() + 6)
+if (today < todayPlus2) {
+
+    const difference = todayPlus2.getTime() - today.getTime();
+    const seconds = Math.round(difference / 1000);
+    const minutes = Math.round(seconds / 60);
+    const hours = Math.round(minutes / 60);
+    const days = Math.round(hours / 24);
+
+    const newDiv =  document.createElement('div');
+    newDiv.innerText = 'Entre le jour un et le jour deux il y a environ :'+ '\n' +seconds+ 's, soit :' +' \n'
+    +minutes+ 'min, soit :' + '\n' +hours+ 'h, soit :' + '\n' +days+ 'j.';
+    document.body.appendChild(newDiv);
+}
